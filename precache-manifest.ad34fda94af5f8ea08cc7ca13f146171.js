8d8b64e8e7f77da32b09362de1d2d364fd093bec
Nedra Krifa <nedrakrifa@gmail.com>
2020-09-15 20:31:02 +0200

    Updates

---
+++ precache-manifest.ad34fda94af5f8ea08cc7ca13f146171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7701f44d029f80b619c8e36525338cc6",
+    "url": "/React-Toast/index.html"
+  },
+  {
+    "revision": "7e3708217c4060986350",
+    "url": "/React-Toast/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b6439939f869a8692d1c",
+    "url": "/React-Toast/static/js/2.eec35613.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Toast/static/js/2.eec35613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3708217c4060986350",
+    "url": "/React-Toast/static/js/main.09383410.chunk.js"
+  },
+  {
+    "revision": "009e489054be04ac0572",
+    "url": "/React-Toast/static/js/runtime-main.9dec9ebc.js"
+  }
+]);
